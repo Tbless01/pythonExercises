28b12cae5c15535687f9a478280b50bca7f326f8
Olaitan Blessing <114719772+Tbless01@users.noreply.github.com>
2022-12-05 11:35:11 +0100

    Add files via upload

---
+++ miles_kilometers.py
@@ -0,0 +1,8 @@
+kilometer = 1.609
+miles = 1
+kilometers = 1
+while  (miles <= 10):
+    #print(kilogram)
+    kilometers = miles * kilometer
+    print(miles, " miles makes ", kilometers, " kilometers ")
+    miles += 1
