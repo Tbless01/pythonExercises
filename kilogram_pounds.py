f24744ad4e592a7d4280f2cde6601f2220fac2fa
Olaitan Blessing <114719772+Tbless01@users.noreply.github.com>
2022-12-05 11:32:30 +0100

    Add files via upload

---
+++ kilogram_pounds.py
@@ -0,0 +1,8 @@
+pound = 2.2
+kilogram = 1
+pounds = 1
+while  (kilogram <= 199):
+    #print(kilogram)
+    pounds = pound * kilogram
+    print(kilogram, " kilogram makes ", pounds, " pounds ")
+    kilogram += 2
